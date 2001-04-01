e622fd84cd6b4c8527a010d1a0b3cd9024cc6d6d
burnett <>
2001-04-01 22:25:06 +0000

    Initial import of package designed for user access to Gaudi for analysis and algorithm development

---
+++ src/UserAlg.cxx
@@ -0,0 +1,126 @@
+// $Header: /nfs/slac/g/glast/ground/cvs/UserAlg/src/UserAlg.cxx,v 1.5 2001/03/25 19:10:30 burnett Exp $
+
+// Include files
+// Gaudi system includes
+#include "Gaudi/MessageSvc/MsgStream.h"
+#include "Gaudi/Kernel/AlgFactory.h"
+#include "Gaudi/Interfaces/IDataProviderSvc.h"
+#include "Gaudi/DataSvc/SmartDataPtr.h"
+#include "Gaudi/Algorithm/Algorithm.h"
+
+// Gaudi ntuple interface
+#include "Gaudi/Interfaces/INTupleSvc.h"
+#include "Gaudi/Interfaces/INTuple.h"
+#include "Gaudi/NTupleSvc/NTuple.h"
+
+// if use the gui
+#include "GuiSvc/IGuiSvc.h"
+#include "gui/GuiMgr.h"
+#include "gui/DisplayControl.h"
+
+// TDS class declarations: input data, and McParticle tree
+#include "GlastEvent/data/TdGlastData.h"
+#include "GlastEvent/MonteCarlo/McVertex.h"
+#include "GlastEvent/MonteCarlo/McParticle.h"
+
+// for access to instrument.ini
+#include "GlastSvc/GlastDetSvc/IGlastDetSvc.h"
+#include "xml/IFile.h"
+
+#include <cassert>
+
+// Define the class here instead of in a header file: not needed anywhere but here!
+//------------------------------------------------------------------------------
+/** 
+A simple algorithm.
+
+  
+*/
+class UserAlg : public Algorithm {
+public:
+    UserAlg(const std::string& name, ISvcLocator* pSvcLocator);
+    StatusCode initialize();
+    StatusCode execute();
+    StatusCode finalize();
+    
+private: 
+    //! number of times called
+    int m_count; 
+    //! the GlastDetSvc used for access to detector info
+    IGlastDetSvc*    m_detSvc; 
+    //! constants from the "instrument.xml" file.
+    xml::IFile * m_ini; 
+    //! access to the Gui Service for display of 3-d objects
+    IGuiSvc*    m_guiSvc;
+    
+};
+//------------------------------------------------------------------------
+
+// necessary to define a Factory for this algorithm
+// expect that the xxx_load.cxx file contains a call     
+//     DLL_DECL_ALGORITHM( UserAlg );
+
+static const AlgFactory<UserAlg>  Factory;
+const IAlgFactory& UserAlgFactory = Factory;
+
+//------------------------------------------------------------------------
+//! ctor
+UserAlg::UserAlg(const std::string& name, ISvcLocator* pSvcLocator)
+:Algorithm(name, pSvcLocator)
+,m_count(0), m_detSvc(0), m_ini(0), m_guiSvc(0)
+{
+    // declare properties with setProperties calls
+    
+}
+//------------------------------------------------------------------------
+//! set parameters and attach to various perhaps useful services.
+StatusCode UserAlg::initialize(){
+    StatusCode  sc = StatusCode::SUCCESS;
+    MsgStream log(msgSvc(), name());
+    log << MSG::INFO << "initialize" << endreq;
+    
+    // Use the Job options service to set the Algorithm's parameters
+    setProperties();
+
+    // now try to find the GlastDevSvc service
+    if (service("GlastDetSvc", m_detSvc).isFailure()){
+        log << MSG::ERROR << "Couldn't find the GlastDetSvc!" << endreq;
+        return StatusCode::FAILURE;
+    }
+    // get the ini file
+    m_ini = const_cast<xml::IFile*>(m_detSvc->iniFile()); //OOPS!
+    assert(4==m_ini->getInt("glast", "xNum"));  // simple check
+
+    // get the Gui service (not required)
+    if (service("GuiSvc", m_guiSvc).isFailure ()){
+        log << MSG::WARNING << "No GuiSvc, so no display" << endreq;
+    }else{
+       //m_guiSvc->guiMgr()->display().add(new Rep, "User rep");
+    }
+
+    return sc;
+}
+
+//------------------------------------------------------------------------
+//! process an event
+StatusCode UserAlg::execute()
+{
+    StatusCode  sc = StatusCode::SUCCESS;
+    MsgStream   log( msgSvc(), name() );
+    log << MSG::INFO << "executing " << ++m_count << " time" << endreq;
+    
+    return sc;
+}
+
+//------------------------------------------------------------------------
+//! clean up, summarize
+StatusCode UserAlg::finalize(){
+    StatusCode  sc = StatusCode::SUCCESS;
+    MsgStream log(msgSvc(), name());
+    log << MSG::INFO << "finalize after " << m_count << " calls." << endreq;
+    
+    return sc;
+}
+
+
+
---
+++ src/mainpage.h
@@ -0,0 +1,26 @@
+//$Header: $
+// (Special "header" just for doxygen)
+
+/*! @mainpage  package userAlg
+
+This package is a "user hook" allowing development of algrorithms in the Glast Gaudi environment
+Since it "uses" the global package pdrApp, it depends on all packages needed to build the 
+simulation/ reconstruction environment. This includes 
+
+  <ul>
+<li> access to all quantities in the Transient Data Store (TDS)</li>
+<li> writing text output to the log file (which can be turned on/off with the gui "printer").
+<li> generating histograms and tuples, which would then be written to a ROOT file. (Need to document this.)
+<li> ability to add components to the display, via GuiSvc.
+</ul>
+  The primary component (which can be easily added to) is UserAlg.
+
+
+  <hr>
+  \section jobOptions src/jobOptions.txt
+  \include jobOptions.txt
+  \section requirements requirements
+  \include requirements
+
+
+*/
