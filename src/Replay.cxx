fbf1123b7cb389c2aa638e0e11a08f5b79f29a40
burnett <>
2002-07-02 19:39:56 +0000

    Implement spec of list of Algs to call.

--- src/Replay.cxx
+++ src/Replay.cxx
@@ -17,12 +17,11 @@
 /** @class Replay 
  * @brief Call (replay?) a given algorithm by name, settable from job options
  * 
- * $Header: /nfs/slac/g/glast/ground/cvs/userAlg/src/Replay.cxx,v 1.1 2002/07/01 22:34:58 burnett Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/userAlg/src/Replay.cxx,v 1.2 2002/07/02 17:15:05 burnett Exp $
  * <br> Following is example of how to select multiple algs:
 <pre>
-  ApplicationMgr.TopAlg += {"Replay/button1", "Replay/button2"};
-  button1.algname="TkrReconAlg";
-  button2.algname="TkrFindAlg";
+  ApplicationMgr.TopAlg += {"Replay"};
+  Replay.Members = {"TkrReconAlg", "TkrFindAlg"};
  </pre>
 
 */
@@ -35,11 +34,16 @@ public:
     StatusCode finalize(){return StatusCode::SUCCESS;}
     
 private:
+    StatusCode addButton(const std::string & name);
+
     // Data Members
 
-    IAlgorithm* m_alg;
-    StringProperty m_algname;
-    static gui::SubMenu *s_replayMenu;
+    StringProperty m_algname; 
+    StringArrayProperty m_algnameList;
+    IAlgManager* m_AlgMgr;
+
+    // allow more than one instance to add to the same menu
+    static gui::SubMenu* s_replayMenu;
 
 };
 
@@ -52,7 +56,8 @@ Replay::Replay(const std::string& name, ISvcLocator* pSvcLocator)
 :Algorithm(name, pSvcLocator)
 {
     // declare properties with setProperties calls
-    declareProperty("algName",  m_algname);
+    declareProperty("algname", m_algname); // for backward compatibility
+    declareProperty("Members",  m_algnameList);
     
 }
 
@@ -66,23 +71,15 @@ StatusCode Replay::initialize(){
     setProperties();
     
        
-   // find the given algorithm 
-    IAlgManager* theAlgMgr;
+   // Alg manager service for finding algs by name
     if ( (serviceLocator( )->getService( "ApplicationMgr",
         IID_IAlgManager,
-        (IInterface*&)theAlgMgr )).isFailure() ) {
+        (IInterface*&)m_AlgMgr )).isFailure() ) {
         log << MSG::ERROR << " no application manager???" << endreq;
         return StatusCode::FAILURE;
         
     }
     
-
-    if( (theAlgMgr->getAlgorithm(m_algname,  m_alg)).isFailure() ) {
-        log << MSG::WARNING 
-            << "Did not find the Algorithm "<< m_algname  << endreq;
-        return StatusCode::FAILURE;
-    }
-
     // get the  Gui service, required for interactive function
     //
     IGuiSvc* guiSvc=0;
@@ -98,19 +95,45 @@ StatusCode Replay::initialize(){
         s_replayMenu = & guiMgr->menu().subMenu("Replay");
     }
 
+
+    // add the button(s)
+    if( m_algname.value() != "") sc = addButton(m_algname);
+
+   std::vector<std::string> names = m_algnameList.value();
+   for( std::vector<std::string>::const_iterator it=names.begin(); it!=names.end(); ++it){
+       if( addButton(*it).isFailure() ) return StatusCode::FAILURE;
+   }
+
+   return sc;
+}
+
+StatusCode Replay::addButton(const std::string & algname)
+{
+    MsgStream log(msgSvc(), name());
+
+    IAlgorithm * alg=0; 
+
+    if( (m_AlgMgr->getAlgorithm(algname,  alg)).isFailure() ) {
+        log << MSG::ERROR 
+            << "Did not find the Algorithm "<< algname  << endreq;
+        return StatusCode::FAILURE;
+    }
+
     // define the recall class right here: all it does is call the alg.
     class ReplayCommand: public gui::Command {
     public:
         ReplayCommand(IAlgorithm* alg):m_alg(alg){}
         void execute(){
+            m_alg->resetExecuted();
             m_alg->execute();
         }
     private:
         IAlgorithm * m_alg;
     };
-    s_replayMenu->addButton(m_algname, new ReplayCommand(m_alg));
 
-   return sc;
+    s_replayMenu->addButton(algname, new ReplayCommand(alg));
+    return StatusCode::SUCCESS;
+
 }
 
 StatusCode Replay::execute()
